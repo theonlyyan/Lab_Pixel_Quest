c6ef47a445bdb6021a9d15897fe80800c54f43c9
theonlyyan <codingisgood12146@gmail.com>
2025-02-15 11:12:06 -0500

    geocontrol

---
+++ Assets/Geo_Quest/Scripts/GeoControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoControl : MonoBehaviour
+{
+    string String = "Hello ";
+    int burger = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+         string String2 = "World";
+        Debug.Log(String + String2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(burger);
+        burger++;
+    }
+}
