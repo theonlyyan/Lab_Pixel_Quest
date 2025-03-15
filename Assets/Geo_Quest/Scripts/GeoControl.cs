1afb4a3028f285720d296ca8c04363a1373543c8
theonlyyan <codingisgood12146@gmail.com>
2025-03-15 10:22:57 -0400

    pixel quest start

--- Assets/Geo_Quest/Scripts/GeoControl.cs
+++ Assets/Geo_Quest/Scripts/GeoControl.cs
@@ -83,7 +83,7 @@ public class GeoControl : MonoBehaviour
                     SceneManager.LoadScene(thisLevel);
                     break;
                 }
-           case "Finsh":
+           case "Finish":
            
             {
                 SceneManager.LoadScene(nextLevel);
